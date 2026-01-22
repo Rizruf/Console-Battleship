4a60a77cbb773ecc8666cd7965b9fd0cdf7f9103
Rizruf <sova_nikeo@mail.ru>
2026-01-22 22:17:22 +0600

    Init: CellState enum and Coordinate struct. Ship class implementation (Core Logic)

---
+++ BattleshipGame/Entities/Ship.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipGame.Entities
+{
+    public class Ship
+    {
+        public string Name {get;}
+        public int Length { get;}
+        public int Hit { get; private set; }
+
+        public List <Coordinate> coordinates { get; set; } = new List <Coordinate> ();
+
+        public Ship(string name, int lengthShip)
+        {
+            Name = name;
+            Length = lengthShip;
+        }
+
+        public bool GotHit(Coordinate shot)
+        {
+            foreach (Coordinate position in coordinates)
+            {
+                if (shot.X == position.X && shot.Y == position.Y)
+                {
+                    Hit++;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsSunk()
+        {
+            if (Hit >= Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ BattleshipGame/Enums/CellState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipGame.Enums
+{
+    public enum CellState
+    {
+        Empty,      // Вода
+        Ship,       // Корабль (невидимый для врага)
+        Miss,       // Промах
+        Hit,        // Попадание (ранен)
+        Sunk        // Убит (весь корабль уничтожен)
+    }
+}
