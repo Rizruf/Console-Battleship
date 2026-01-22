65c38e5c07d5a2d3d290acf4a756592595b0e39a
Rizruf <sova_nikeo@mail.ru>
2026-01-22 21:03:16 +0600

    Init: Project structure

---
+++ BattleshipGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleshipGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
