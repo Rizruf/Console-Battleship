caaffebc173156163b1bcf819fc40c7f8bdbe648
Rizruf <sova_nikeo@mail.ru>
2026-01-23 21:55:52 +0600

    Successful integration test. Engine is ready for production

--- BattleshipGame/Program.cs
+++ BattleshipGame/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace BattleshipGame
+﻿using BattleshipGame.Entities;
+using BattleshipGame.GameEngine;
+using BattleshipGame.UI;
+
+namespace BattleshipGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Board myBoard = new Board();
+
+            Ship destroyer = new Ship("Destroyer", 2);
+
+            myBoard.PlaceShip(destroyer, new Coordinate(1, 0), true);
+
+            Console.WriteLine("Стреляем в A1...");
+            myBoard.Fire(new Coordinate(0, 0));
+
+            Console.WriteLine("Стреляем в B1...");
+            myBoard.Fire(new Coordinate(1, 0));
+
+            Console.WriteLine("Стреляем в B2...");
+            myBoard.Fire(new Coordinate(1, 1));
+
+            Console.Clear(); 
+            ConsoleView.DrawBoard(myBoard);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ BattleshipGame/UI/ConsoleView.cs
@@ -0,0 +1,57 @@
+﻿using BattleshipGame.Entities;
+using BattleshipGame.Enums;
+using BattleshipGame.GameEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipGame.UI
+{
+    internal static class ConsoleView
+    {
+        public static void DrawBoard(Board board)
+        {
+            Console.Write("   ");
+            Console.WriteLine("A B C D E F G H I J");
+
+            for (int y = 0; y < 10; y++)
+            {
+                if (y < 9) Console.Write($" {y + 1} ");
+                else Console.Write($"{y + 1} ");
+
+                for (int x = 0; x < 10; x++)
+                {
+                    CellState currentCell = board.Map[x, y];
+
+                    switch (currentCell)
+                    {
+                        case CellState.Empty:
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("~ ");
+                            break;
+                        case CellState.Ship:
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.Write("O ");
+                            break;
+                        case CellState.Miss:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("x ");
+                            break;
+                        case CellState.Hit:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("Ж ");
+                            break;
+                        case CellState.Sunk:
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write("W ");
+                            break;
+                    }
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
