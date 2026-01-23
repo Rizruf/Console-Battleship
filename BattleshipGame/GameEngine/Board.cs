b20ebb62606186aaaca467ebfd1542818ffe5fc9
Rizruf <sova_nikeo@mail.ru>
2026-01-23 20:47:25 +0600

    Feat: Implemented firing logic with hit/miss check, as well as implemented a field for ships

---
+++ BattleshipGame/GameEngine/Board.cs
@@ -0,0 +1,67 @@
+﻿using BattleshipGame.Entities;
+using BattleshipGame.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipGame.GameEngine
+{
+    public class Board
+    {
+        public CellState[,] Map { get; set; } = new CellState[10, 10];
+
+        public List<Ship> Fleet { get; set; } = new List<Ship>();
+
+        public void ResetBoard()
+        {
+            for (int i = 0; i < Map.GetLength(0); i++)
+            {
+                for (int j = 0; j < Map.GetLength(1); j++)
+                {
+                    Map[i, j] = CellState.Empty;
+                }
+            }
+        }
+
+        public Board()
+        {
+            ResetBoard();
+        }
+
+        public CellState Fire(Coordinate coordinate)
+        {
+            if (coordinate.X > 9 || coordinate.Y > 9 || coordinate.X < 0 || coordinate.Y < 0)
+            {
+                throw new Exception("Координаты за пределами поля!");
+            }
+
+            if (Map[coordinate.X, coordinate.Y] != CellState.Empty
+                && Map[coordinate.X, coordinate.Y] != CellState.Ship)
+            {
+                return Map[coordinate.X, coordinate.Y];
+            }
+
+            if (Map[coordinate.X, coordinate.Y] != CellState.Empty
+                && Map[coordinate.X, coordinate.Y] == CellState.Ship)
+            {
+                foreach (var ship in Fleet)
+                {
+                    if (ship.GotHit(coordinate))
+                    {
+                        CellState newState = ship.IsSunk() ? CellState.Sunk : CellState.Hit;
+
+                        Map[coordinate.X, coordinate.Y] = newState;
+                        return newState;
+                    }
+                }
+            }
+
+            Map[coordinate.X, coordinate.Y] = CellState.Miss;
+            return CellState.Miss;
+        }
+    }
+}
